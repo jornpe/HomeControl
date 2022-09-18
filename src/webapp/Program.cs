96a3c3287b4ebf45ba3d3696e5885f00117a8831
Jørn Pettersen <joern.pettersen@gmail.com>
2022-09-18 16:29:00 +0200

    Connecting to correct endpoint based on app config

--- src/webapp/Program.cs
+++ src/webapp/Program.cs
@@ -5,7 +5,13 @@ using Microsoft.Azure.Functions.Authentication.WebAssembly;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+if (!Uri.TryCreate(builder.Configuration.GetValue<string>("API_ENDPOINT"), UriKind.Absolute, out var apiEndpoint))
+{
+    throw new InvalidOperationException($"App configurationURI is not valid. URI: ${apiEndpoint}");
+}
+
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = apiEndpoint });
+
 builder.Services.AddStaticWebAppsAuthentication();
 
 await builder.Build().RunAsync();
